33c0e79d99e51a8377a4ffd59478b3f992463b15
Phillip Lord <phillip.lord@newcastle.ac.uk>
2021-10-10 18:51:59 +0100

    Support addressing courses by their code
    Previously, courses had to be called either by their identifiers or
    substring matching of their names which breaks between one year and
    the next. course_code solves that problem.

--- tent_pole/course.py
+++ tent_pole/course.py
@@ -5,15 +5,22 @@ from . import config
 def __get_courses():
     return config.config_canvas().get_courses()
 
+
 def course_by_name(coursename):
     return next(course for course in __get_courses()
                 if coursename in course.name)
+
+def course_by_code(coursecode):
+    return next(course for course in __get_courses()
+                if coursecode == course.course_code)
 
 def course_by_guess(courseidentifier):
     return (
         courseidentifier.isnumeric()
         and
         config.config_canvas().get_course(courseidentifier)
+        or
+        course_by_code(courseidentifier)
         or
         course_by_name(courseidentifier)
     )
